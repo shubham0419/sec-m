e34139c088ec2da1c0e602a20d9ae00a772db0e9
Your Name <you@example.com>
2024-03-27 09:42:02 +0530

    crud

--- project-d2/app.js
+++ project-d2/app.js
@@ -2,15 +2,31 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 const mongoose = require("mongoose");
+const Product = require("./model/product");
 
+app.set("view engine", "ejs");
+app.set("views", "views");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // mongoose connection
-mongoose.connect("mongodb+srv://shubham21:shubham21@cluster0.k76tzvk.mongodb.net/product")
-.then(()=>{
-    console.log('db connected');
-})
+mongoose.connect("mongodb://localhost:27017/sec-m").then(() => {
+  console.log("db connected");
+});
 
-app.get("/", (req, res) => {	});
+app.get("/product/new", (req, res) => {
+  res.render("addProduct");
+});
+
+app.post("/product/new", async (req, res) => {
+  const { productName, price, description, imageUrl } = req.body;
+  const newProduct = await Product.create({
+    productName,
+    price,
+    description,
+    imageUrl,
+  });
+  newProduct.save();
+  res.redirect("/product/new");
+});
 
 app.listen(PORT, () => console.log("http://localhot/" + PORT));
--- project-d2/model/product.js
+++ project-d2/model/product.js
@@ -1,29 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 // product schema
-const productSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        trim:true,
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    price:{
-        type:Number,
-        required:true,
-        trim:true,
-        min:10
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+      min: 10,
     },
-    des:{
-        type:Number,
-        required:true,
-        trim:true,
+    des: {
+      type: Number,
+      required: true,
+      trim: true,
     },
-    image:{
-        type:String,
-        required:true,
-        trim:true,
-    }
-})
+    image: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  //wiil store created time
+  { timestamps: true }
+);
 // collection creation
-const Product = mongoose.model("Product",productSchema);
+const Product = mongoose.model("Product", productSchema);
 // export
 module.exports = Product;
